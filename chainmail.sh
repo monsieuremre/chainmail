e91c6a4b6279be0bd6793a2672f8e23df0e1d7c2
monsieuremre <130907164+monsieuremre@users.noreply.github.com>
2023-09-25 11:51:07 +0000

    licensing info

--- chainmail.sh
+++ chainmail.sh
@@ -1,3 +1,11 @@
+## This file is part of Chainmail <https://github.com/monsieuremre/chainmail>
+## Chainmail is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+## License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+## version. RAETSELx86 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+## implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details. You should have received a copy of the GNU General Public License along with this program. If not, see
+## <https://www.gnu.org/licenses/>.
+
 #!/bin/bash
 
 update_everything() {
